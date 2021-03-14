24e177c7ee6c066851a6e79fb27ccda04d4fa3c7
Dane Murphy <danem7@gmail.com>
2021-03-14 12:40:32 -0700

    sequelize logging added

--- src/db.js
+++ src/db.js
@@ -3,10 +3,9 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB, process.env.PG_USER, process.env.PG_PASSWORD, {
 	dialect: 'postgres',
 	host: process.env.HOST,
-	logging: false
+	logging: true
 });
 
 sequelize.sync();
 
 module.exports = sequelize;
-// small change
